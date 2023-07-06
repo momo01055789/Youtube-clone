d61382849e99b27c8897f4af61d8f12c5119d2f4
momo01055789 <mohamed.elhadidy666@gmail.com>
2023-07-06 17:31:08 +0300

    remove .env file from git

---
+++ src/Components/VideoCard.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constatns";
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card
+      sx={{
+        alignItems: "center",
+        justifyContent: "center",
+        width: { xs: "100%", sm: "290px", md: "310px" },
+        boxShadow: "none",
+        borderRadius: 0,
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
+          alt={snippet?.title}
+          sx={{
+            alignItems: "center",
+            justifyContent: "center",
+            width: { xs: "100%", md: "310px" },
+            height: 180,
+          }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircleIcon
+              sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+            />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default VideoCard;
